863d739cb26c16efd72aae0316949b75c9c15193
2002Parcidio <pasquiparcidio@gmail.com>
2022-04-01 16:28:41 +0100

    cancel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 function App() {
+  const name = 'Parcídio'
+  const url = 'https://via.placeholder.com/500'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Pessoa nome='Indefinido' idade='15' profissão='Pedreiro'/>
+      <Pessoa idade='15'/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <HelloWord/>
+        <SayMyName nome='ParcídioThegreat'/>
+
     </div>
   );
 }
